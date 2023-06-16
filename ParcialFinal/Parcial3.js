04c4d6e005b99200bc0cd51adbffbb11966e06f2
Farid0623 <132091536+Farid0623@users.noreply.github.com>
2023-06-16 11:01:23 -0500

    Add files via upload

---
+++ ParcialFinal/Parcial3.js
@@ -0,0 +1,33 @@
+let dulces = [
+    {
+        "tipo": "dulce",
+        "color": "azul",
+        "marca": "Chocobreak",
+        "relleno": "fresa",
+        "textura": "suave",
+
+    },
+    {
+        "tipo": "dulce ácido",
+        "color": "Multicolor",
+        "marca": "Barrilete",
+        "relleno": "azúcar",
+        "textura": "dura",
+    },{
+        "tipo": "dulce",
+        "color": "azul",
+        "marca": "Chocobreak",
+        "relleno": "fresa",
+        "textura": "suave",
+    },
+    {
+        "tipo": "dulce salado",
+        "color": "azul y rojo",
+        "marca": "Mani-moto",
+        "relleno": "maní",
+        "textura": "crocante",
+    }
+    
+]
+
+console.log (VentaDulces)
